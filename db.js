0906867f01f536cecd8a403d0481c343e377e020
HAITHAM-HK4 <bmwgt32@gmail.com>
2026-02-28 23:14:52 +0300

    Update db.js

--- db.js
+++ db.js
@@ -2,14 +2,14 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host:     process.env.DB_HOST,
-    port:     process.env.DB_PORT || 3306,
-    user:     process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host:     process.env.MYSQLHOST,
+    port:     process.env.MYSQLPORT || 3306,
+    user:     process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQL_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
-    ssl: { rejectUnauthorized: false }  // مطلوب على Railway
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
