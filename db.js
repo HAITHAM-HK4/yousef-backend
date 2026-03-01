c286c07babd7cc0e1196e124eb8f89b2bbda7917
HAITHAM-HK4 <bmwgt32@gmail.com>
2026-03-01 23:25:09 +0300

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     port:     process.env.MYSQLPORT || 3306,
     user:     process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: 'railway',
     waitForConnections: true,
     connectionLimit: 10,
     ssl: { rejectUnauthorized: false }
